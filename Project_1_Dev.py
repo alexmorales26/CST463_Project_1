31ff816ba6940132dd3d08425b32ec3dbcbe49a8
Nolan Guzman <nolanguzman@Nolans-MacBook-Pro.local>
2018-10-15 14:07:14 -0700

    Read In Data

--- Project_1_Dev.py
+++ Project_1_Dev.py
@@ -5,3 +5,7 @@ Created on Wed Oct 10 17:51:36 2018
 @author: Alexander Morales, Nolan Guzman, & Alyssia Goodwin
 """
 
+import pandas as pd
+import numpy as np
+
+dat = pd.read_csv('Desktop/CST463_Project_1/default_cc_train.csv')
